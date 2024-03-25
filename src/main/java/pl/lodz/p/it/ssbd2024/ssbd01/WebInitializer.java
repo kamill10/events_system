5360c1713b9367307e04205ab0699268b32c9686
242506 Przybylski Adam <242506@edu.p.lodz.pl>
2024-03-25 20:17:34 +0100

    Konfiguracja projektu startego w Spring
    Pierwsza wersja kodu aplikacji sieciowej

---
+++ src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/WebInitializer.java
@@ -0,0 +1,35 @@
+package pl.lodz.p.it.ssbd2024.ssbd01;
+
+import jakarta.servlet.Filter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import pl.lodz.p.it.ssbd2024.ssbd01.config.RootConfig;
+import pl.lodz.p.it.ssbd2024.ssbd01.config.WebConfig;
+
+import java.nio.charset.StandardCharsets;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        final CharacterEncodingFilter cef = new CharacterEncodingFilter();
+        cef.setEncoding(StandardCharsets.UTF_8.name());
+        cef.setForceEncoding(true);
+        return new Filter[]{new HiddenHttpMethodFilter(), cef};
+    }
+}
---
+++ src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/config/WebConfig.java
@@ -0,0 +1,34 @@
+package pl.lodz.p.it.ssbd2024.ssbd01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.lodz.p.it.ssbd2024.ssbd01")
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Override
+    public Validator getValidator() {
+        return WebMvcConfigurer.super.getValidator();
+    }
+}
