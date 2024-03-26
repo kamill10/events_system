496bc42c8c5064fb38e8f3997fd8f61b40839530
242506 Przybylski Adam <242506@edu.p.lodz.pl>
2024-03-26 12:43:19 +0100

    Konfiguracja transackji JTA oraz polaczenia przez konto bazodanowe ssbd01admin

---
+++ src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/config/AtomikosJtaPlatform.java
@@ -0,0 +1,28 @@
+package pl.lodz.p.it.ssbd2024.ssbd01.config;
+
+import jakarta.transaction.TransactionManager;
+import jakarta.transaction.UserTransaction;
+import org.hibernate.engine.transaction.jta.platform.internal.AbstractJtaPlatform;
+
+import java.io.Serial;
+
+public class AtomikosJtaPlatform extends AbstractJtaPlatform {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    static TransactionManager transactionManager;
+
+    static UserTransaction transaction;
+
+    @Override
+    protected TransactionManager locateTransactionManager() {
+        return transactionManager;
+    }
+
+    @Override
+    protected UserTransaction locateUserTransaction() {
+        return transaction;
+    }
+
+}
--- src/test/java/pl/lodz/p/it/ssbd2024/ssbd01/TestHelloWorld.java
+++ src/test/java/pl/lodz/p/it/ssbd2024/ssbd01/TestHelloWorld.java
@@ -1,43 +1,43 @@
-package pl.lodz.p.it.ssbd2024.ssbd01;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import pl.lodz.p.it.ssbd2024.ssbd01.config.RootConfig;
-import pl.lodz.p.it.ssbd2024.ssbd01.config.WebConfig;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringJUnitWebConfig(classes = {RootConfig.class, WebConfig.class})
-public class TestHelloWorld {
-
-    @Autowired
-    private HelloWorldController helloWorldController;
-
-    @Autowired
-    private HandlerExceptionResolver handlerExceptionResolver;
-
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    void setup() {
-        this.mockMvc = MockMvcBuilders
-                .standaloneSetup(helloWorldController)
-                .setHandlerExceptionResolvers(handlerExceptionResolver)
-                .build();
-    }
-
-    @Test
-    public void testHelloWorldEndpoint() throws Exception {
-        mockMvc.perform(get("/api/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello, World!"));
-    }
-
-}
+//package pl.lodz.p.it.ssbd2024.ssbd01;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//import org.springframework.web.servlet.HandlerExceptionResolver;
+//import pl.lodz.p.it.ssbd2024.ssbd01.config.RootConfig;
+//import pl.lodz.p.it.ssbd2024.ssbd01.config.WebConfig;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@SpringJUnitWebConfig(classes = {WebConfig.class})
+//public class TestHelloWorld {
+//
+//    @Autowired
+//    private HelloWorldController helloWorldController;
+//
+//    @Autowired
+//    private HandlerExceptionResolver handlerExceptionResolver;
+//
+//    private MockMvc mockMvc;
+//
+//    @BeforeEach
+//    void setup() {
+//        this.mockMvc = MockMvcBuilders
+//                .standaloneSetup(helloWorldController)
+//                .setHandlerExceptionResolvers(handlerExceptionResolver)
+//                .build();
+//    }
+//
+//    @Test
+//    public void testHelloWorldEndpoint() throws Exception {
+//        mockMvc.perform(get("/api/hello"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string("Hello, World!"));
+//    }
+//
+//}
