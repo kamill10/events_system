4a7c6e6cb933a23cb3ada97e18eb5526caf1e04b
242369 Cieślak Kamil <kam.cieslak@o2.pl>
2024-04-17 21:10:34 +0200

    security config changes

--- src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/config/security/SecurityConfig.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/config/security/SecurityConfig.java
@@ -19,8 +19,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
