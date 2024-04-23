1cc609168fa2c3e402f235d81f2286344a84c5aa
242499 Piotr Płeska <242499@edu.p.lodz.pl>
2024-04-23 23:49:09 +0200

    Poprawa zaciagnietych bibliotek

--- src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/config/WebConfig.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/config/WebConfig.java
@@ -1,6 +1,9 @@
 package pl.lodz.p.it.ssbd2024.ssbd01.config;
 
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.validation.Validator;
