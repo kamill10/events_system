cd618f6c91f278672a2edd72021f3b1e1481bbaa
242499 Piotr Płeska <242499@edu.p.lodz.pl>
2024-04-27 20:59:51 +0200

    Stworzenie encji i repozytorium dla potwierdzenia konta

---
+++ src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/mok/repository/AccountConfirmationRepository.java
@@ -0,0 +1,16 @@
+package pl.lodz.p.it.ssbd2024.ssbd01.mok.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import pl.lodz.p.it.ssbd2024.ssbd01.entity.mok.AccountConfirmation;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Transactional(propagation = Propagation.MANDATORY)
+public interface AccountConfirmationRepository extends JpaRepository<AccountConfirmation, UUID> {
+
+    Optional<AccountConfirmation> findByToken(String token);
+
+}
