003f3287dd526ec6513c88275fcd43e64a3e9b05
MFamoeba <161942152+MFamoeba@users.noreply.github.com>
2024-05-30 14:37:07 +0200

    password values removed from dto toString

--- src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/dto/update/UpdatePasswordDTO.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/dto/update/UpdatePasswordDTO.java
@@ -1,12 +1,17 @@
 package pl.lodz.p.it.ssbd2024.ssbd01.dto.update;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import pl.lodz.p.it.ssbd2024.ssbd01.util.messages.ExceptionMessages;
 
 public record UpdatePasswordDTO(
 
         @Size(min = 8, max = 72, message = ExceptionMessages.INCORRECT_PASSWORD)
-        String value
+        String password
 ) {
+        @Override
+        public String toString() {
+                return "UpdatePasswordDTO{" +
+                        "password='********'" +
+                        '}';
+        }
 }
