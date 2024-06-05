df46d5457e3d4d6ab6f22c0745415e6a7f2b2bb7
Jakub Pazio <242489@edu.p.lodz.pl>
2024-06-05 12:38:26 +0200

    endpoint fix

--- src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/mow/controller/EventController.java
+++ src/main/java/pl/lodz/p/it/ssbd2024/ssbd01/mow/controller/EventController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/events")
+@RequestMapping("/api/events")
 @RequiredArgsConstructor
 public class EventController {
 
