29f302c3def0d84d736fa15b2aea2cc385670e41
242499 Płeska Piotr <242499@edu.p.lodz.pl>
2024-06-08 02:01:13 +0200

    Poprawa Location.ts po scaleniu

--- ssbd202401web/src/types/Location.ts
+++ ssbd202401web/src/types/Location.ts
@@ -12,6 +12,7 @@ export interface Location {
 
 export interface PaginationLocationResponse
   extends PaginationResponse<Location> {}
+
 export interface CreateLocation {
   name: string;
   city: string;
@@ -19,35 +20,6 @@ export interface CreateLocation {
   street: string;
   buildingNumber: string;
   postalCode: string;
-}
-
-export interface Sort {
-  empty: boolean;
-  sorted: boolean;
-  unsorted: boolean;
-}
-
-export interface Pageable {
-  pageNumber: number;
-  pageSize: number;
-  sort: Sort;
-  offset: number;
-  paged: boolean;
-  unpaged: boolean;
-}
-
-export interface PaginationLocationResponse {
-  content: Location[];
-  pageable: Pageable;
-  last: boolean;
-  totalElements: number;
-  totalPages: number;
-  size: number;
-  number: number;
-  sort: Sort;
-  first: boolean;
-  numberOfElements: number;
-  empty: boolean;
 }
 
 export interface LocationsWithNumberOfElements {
