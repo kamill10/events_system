b24b1e886aa1a7e66285073cc4b3ac5a810b55ce
242506 Przybylski Adam <242506@edu.p.lodz.pl>
2024-06-12 02:14:52 +0200

    Optymalizacja testów (kontenery wstaj tylko raz)

--- src/test/java/pl/lodz/p/it/ssbd2024/ssbd01/integration/AuthenticationControllerIT.java
+++ src/test/java/pl/lodz/p/it/ssbd2024/ssbd01/integration/AuthenticationControllerIT.java
@@ -1,148 +1,30 @@
 package pl.lodz.p.it.ssbd2024.ssbd01.integration;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.restassured.response.ValidatableResponse;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.testcontainers.containers.BindMode;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.Network;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.MountableFile;
 import pl.lodz.p.it.ssbd2024.ssbd01.dto.mok.LoginDTO;
 import pl.lodz.p.it.ssbd2024.ssbd01.dto.mok.create.CreateAccountDTO;
 import pl.lodz.p.it.ssbd2024.ssbd01.util._enum.LanguageEnum;
 
-import java.io.IOException;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 
-@Testcontainers
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class AuthenticationControllerIT {
 
-    static Network network = Network.newNetwork();
-
-    static ObjectMapper objectMapper;
-
-    static int port;
-
-    static String baseUrl;
-
-    static String adminToken;
-
-    static String participantToken;
-
-    static String managerToken;
-
-    public static ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper;
-    }
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.2")
-            .withDatabaseName("ssbd01")
-            .withUsername("ssbd01admin")
-            .withPassword("admin")
-            .withExposedPorts(5432)
-            .withNetworkAliases("postgres")
-            .withNetwork(network)
-            .withInitScript("sql/create-users.sql")
-            .withCopyFileToContainer(
-                    MountableFile.forClasspathResource("sql/init-test-data.sql"),
-                    "/tmp/init-data.sql"
-            )
-            .withCopyFileToContainer(
-                    MountableFile.forClasspathResource("sql/delete-data.sql"),
-                    "/tmp/delete-data.sql"
-            )
-            .waitingFor(Wait.forSuccessfulCommand("pg_isready -U ssbd01admin"))
-            .withReuse(true);
-
-    @Container
-    static GenericContainer<?> tomcat = new GenericContainer<>("tomcat:10.1.19-jre21")
-            .withExposedPorts(8080)
-            .withNetworkAliases("tomcat")
-            .withNetwork(network)
-            .dependsOn(postgres)
-            .withCopyFileToContainer(
-                    MountableFile.forHostPath("target/ssbd01.war"),
-                    "/usr/local/tomcat/webapps/ssbd01.war"
-            )
-            .waitingFor(Wait.forHttp("/ssbd01/api/accounts").forStatusCode(403))
-            .withReuse(true)
-            .withFileSystemBind("transactions.log", "/usr/local/tomcat/transactions.log", BindMode.READ_WRITE)
-            .withFileSystemBind("auth.log", "/usr/local/tomcat/auth.log", BindMode.READ_WRITE)
-            .withFileSystemBind("all_method.log", "/usr/local/tomcat/all_method.log", BindMode.READ_WRITE);
-
+public class AuthenticationControllerIT extends AbstractControllerIT {
 
     @BeforeEach
-    public void initData() throws IOException, InterruptedException {
-        port = tomcat.getMappedPort(8080);
-        baseUrl = "http://localhost:" + port + "/ssbd01/api";
-        objectMapper = objectMapper();
-        postgres.execInContainer("psql", "-U", "ssbd01admin", "ssbd01", "-f",
-                "/tmp/delete-data.sql");
-        postgres.execInContainer("psql", "-U", "ssbd01admin", "ssbd01", "-f",
-                "/tmp/init-data.sql");
-    }
-
-    @Test
-    @Order(1)
-    public void authenticationTest() throws JsonProcessingException {
-        LoginDTO loginDTO = new LoginDTO("testAdmin", "P@ssw0rd");
-
-        ValidatableResponse response = given()
-                .contentType("application/json")
-                .header(HttpHeaders.ACCEPT_LANGUAGE, "en")
-                .body(objectMapper.writeValueAsString(loginDTO))
-                .when()
-                .post(baseUrl + "/auth/authenticate")
-                .then()
-                .statusCode(HttpStatus.OK.value());
-
-        String jwtToken = response.extract().body().asString();
-        adminToken = jwtToken.substring(1, jwtToken.length() - 1);
-
-        loginDTO = new LoginDTO("testParticipant", "P@ssw0rd");
-        response = given()
-                .contentType("application/json")
-                .header(HttpHeaders.ACCEPT_LANGUAGE, "en")
-                .body(objectMapper.writeValueAsString(loginDTO))
-                .when()
-                .post(baseUrl + "/auth/authenticate")
-                .then()
-                .statusCode(HttpStatus.OK.value());
-
-        jwtToken = response.extract().body().asString();
-        participantToken = jwtToken.substring(1, jwtToken.length() - 1);
-
-        loginDTO = new LoginDTO("testManager", "P@ssw0rd");
-        response = given()
-                .contentType("application/json")
-                .header(HttpHeaders.ACCEPT_LANGUAGE, "en")
-                .body(objectMapper.writeValueAsString(loginDTO))
-                .when()
-                .post(baseUrl + "/auth/authenticate")
-                .then()
-                .statusCode(HttpStatus.OK.value());
-
-        jwtToken = response.extract().body().asString();
-        managerToken = jwtToken.substring(1, jwtToken.length() - 1);
+    public void authenticate() throws JsonProcessingException {
+        authenticationToAdminTest();
     }
 
     @Test
     public void testRegisterAccount() {
-        CreateAccountDTO createAccountDTO = new CreateAccountDTO("user", "P@ssw0rd", "isrpgrupa1@proton.me", 1, "firstName", "lastName", LanguageEnum.POLISH);
+        CreateAccountDTO createAccountDTO =
+                new CreateAccountDTO("user", "P@ssw0rd", "isrpgrupa1@proton.me", 1, "firstName", "lastName", LanguageEnum.POLISH);
         given()
                 .contentType("application/json")
                 .body(createAccountDTO)
@@ -161,7 +43,8 @@ public class AuthenticationControllerIT {
 
     @Test
     public void testRegisterAccountWithAnonymousUserName() {
-        CreateAccountDTO createAccountDTO = new CreateAccountDTO("anonymous", "P@ssw0rd", "isrpgrupa2@proton.me", 1, "firstName", "lastName", LanguageEnum.POLISH);
+        CreateAccountDTO createAccountDTO =
+                new CreateAccountDTO("anonymous", "P@ssw0rd", "isrpgrupa2@proton.me", 1, "firstName", "lastName", LanguageEnum.POLISH);
         given()
                 .contentType("application/json")
                 .body(createAccountDTO)
@@ -187,7 +70,8 @@ public class AuthenticationControllerIT {
 
     @Test
     public void testRegisterAccountWithNotUniqueUsername() {
-        CreateAccountDTO createAccountDTO = new CreateAccountDTO("testAdmin", "P@ssw0rd", "isrpgrupa1@proton.me", 1, "firstName", "lastName", LanguageEnum.POLISH);
+        CreateAccountDTO createAccountDTO =
+                new CreateAccountDTO("testAdmin", "P@ssw0rd", "isrpgrupa1@proton.me", 1, "firstName", "lastName", LanguageEnum.POLISH);
         given()
                 .contentType("application/json")
                 .body(createAccountDTO)
@@ -222,7 +106,7 @@ public class AuthenticationControllerIT {
 
     @Test
     public void failedLoginAttemptsTest() {
-        LoginDTO loginDTO = new LoginDTO("testAdmin", "invalidPassword");
+        LoginDTO loginDTO = new LoginDTO("marcinKaczkan21", "invalidPassword");
 
         given()
                 .contentType("application/json")
@@ -251,7 +135,7 @@ public class AuthenticationControllerIT {
                 .then()
                 .statusCode(HttpStatus.FORBIDDEN.value());
 
-        LoginDTO newLoginDto = new LoginDTO("testAdmin", "P@ssw0rd");
+        LoginDTO newLoginDto = new LoginDTO("marcinKaczkan21", "P@ssw0rd");
 
         ValidatableResponse response = given()
                 .contentType("application/json")
@@ -279,9 +163,22 @@ public class AuthenticationControllerIT {
     }
 
     @Test
-    public void logoutTest() {
+    public void logoutTest() throws JsonProcessingException {
+        LoginDTO loginDTO = new LoginDTO("jacekTeler23", "P@ssw0rd");
+        ValidatableResponse response = given()
+                .contentType("application/json")
+                .header(HttpHeaders.ACCEPT_LANGUAGE, "en")
+                .body(objectMapper.writeValueAsString(loginDTO))
+                .when()
+                .post(baseUrl + "/auth/authenticate")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+
+        String jwtToken = response.extract().body().asString();
+        String token = jwtToken.substring(1, jwtToken.length() - 1);
+
         given()
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + adminToken)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .when()
                 .post(baseUrl + "/auth/logout")
                 .then()
--- src/test/resources/sql/delete-data.sql
+++ src/test/resources/sql/delete-data.sql
@@ -1,17 +1,45 @@
-DELETE FROM public.account_role;
-DELETE FROM public.password_history;
-DELETE FROM public.password_reset;
-DELETE FROM public.accountconfirmation;
-DELETE FROM public.change_email;
-DELETE FROM public.change_my_password;
-DELETE FROM public.confirmation_reminder;
-DELETE FROM public.credential_reset;
-DELETE FROM public.password_history;
-DELETE FROM public.personal_data;
-DELETE FROM public.account;
-DELETE FROM public.role;
-DELETE FROM public.account_time_zone;
-DELETE FROM public.account_time_theme;
-DELETE FROM public.account_history;
 DELETE FROM public.account_history_role;
+
+DELETE FROM public.account_role;
+
+DELETE FROM public.account_unlock;
+
+DELETE FROM public.accountconfirmation;
+
+DELETE FROM public.change_email;
+
+DELETE FROM public.change_my_password;
+
+DELETE FROM public.confirmation_reminder;
+
+DELETE FROM public.credential_reset;
+
+DELETE FROM public.jwt_whitelist_token;
+
+DELETE FROM public.password_history;
+
+DELETE FROM public.personal_data;
+
 DELETE FROM public.personal_data_history;
+
+DELETE FROM public.account_history;
+
+DELETE FROM public.role;
+
+DELETE FROM public.ticket;
+
+DELETE FROM public.session;
+
+DELETE FROM public.event;
+
+DELETE FROM public.room;
+
+DELETE FROM public.location;
+
+DELETE FROM public.speaker;
+
+DELETE FROM public.account;
+
+DELETE FROM public.account_theme;
+
+DELETE FROM public.account_time_zone;
