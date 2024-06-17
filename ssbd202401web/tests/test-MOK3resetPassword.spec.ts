6dd2cbfc435e87d473cadacf728d3181db0d9f6e
242499 Płeska Piotr <242499@edu.p.lodz.pl>
2024-06-17 18:47:02 +0200

    Dodanie testow e2e MOK3/MOK4/MOK5/MOK6/MOK7

---
+++ ssbd202401web/tests/test-MOK3resetPassword.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('resetPassword', async ({ page }) => {
+  await page.goto('https://team-1.proj-sum.it.p.lodz.pl/');
+  await page.getByRole('menuitem', { name: 'Log in' }).getByRole('paragraph').click();
+  await page.getByRole('link', { name: 'I forgot my password :(' }).click();
+  await page.getByLabel('E-mail').click();
+  await page.getByLabel('E-mail').fill('testMail123@email.com');
+  await page.getByRole('button', { name: 'Change password' }).click();
+  await expect(page.getByText('Request sent successfully!')).toBeVisible();
+});
---
+++ ssbd202401web/tests/test_MOK5unblockAccount.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('unblockAccount', async ({ page }) => {
+  // Przygotowanie odpowiedniego stanu aplikacji
+  await page.goto('https://team-1.proj-sum.it.p.lodz.pl/');
+  await page.getByRole('menuitem', { name: 'Log in' }).getByRole('paragraph').click();
+  await page.getByLabel('Username*').click();
+  await page.getByLabel('Username*').fill('testAdmin');
+  await page.getByLabel('Username*').press('Tab');
+  await page.getByLabel('Password*').fill('P@ssw0rd');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('menuitem', { name: 'Accounts' }).click();
+  await page.getByRole('cell', { name: '08839a96-9432-4ada-a9bd-' }).click();
+  await page.getByRole('tab', { name: 'Change profile details' }).click();
+  await page.getByRole('checkbox').click();
+  await page.getByRole('button', { name: 'Yes' }).click();
+  await expect(page.getByText('Account deactivated successfully!')).toBeVisible();
+  await page.getByRole('tab', { name: 'Profile details', exact: true }).click();
+  await expect(page.getByRole('cell', { name: 'No', exact: true })).toBeVisible();
+
+  // weryfikacja przypadku użycia
+  await page.getByRole('tab', { name: 'Change profile details' }).click();
+  await page.getByRole('checkbox').click();
+  await page.getByRole('button', { name: 'Yes' }).click();
+  await expect(page.getByText('Account activated successfully!')).toBeVisible();
+  await page.getByRole('tab', { name: 'Profile details', exact: true }).click();
+  await expect(page.getByRole('cell', { name: 'Yes', exact: true }).first()).toBeVisible();
+});
