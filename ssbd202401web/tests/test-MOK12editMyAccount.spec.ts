c35dc9d4286150c8628485d514d6d8d674c92b6c
242369 Cieślak Kamil <kam.cieslak@o2.pl>
2024-06-20 18:28:15 +0200

    refaktor testow MOK12,MOK13

--- ssbd202401web/tests/test-MOK12editMyAccount.spec.ts
+++ ssbd202401web/tests/test-MOK12editMyAccount.spec.ts
@@ -1,49 +1,29 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("test", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
-  await page
-    .getByRole("menuitem", { name: "Log in" })
-    .getByRole("paragraph")
-    .click();
-  await page.getByLabel("Username*").click();
-  await page.getByLabel("Username*").click({
-    button: "right",
-  });
-  await page.getByLabel("Username*").fill("halinaposwiatoswka12");
-  await page.getByLabel("Password*").click();
-  await page.getByLabel("Password*").fill("P@ssw0rd");
-  await page.getByLabel("Password*").press("Enter");
-  await page.getByRole("button").nth(1).click();
-  await page.locator("li.MuiButtonBase-root:nth-child(2)").click();
-  await page.locator("button.MuiTab-root:nth-child(2)").click();
-
-  await page.locator("#firstName").click();
-  await page.locator("#firstName").fill("zmiana");
-  await page.locator("#lastName").click();
-  await page.locator("#lastName").fill("nowe");
-  await page.locator("button.MuiButton-root:nth-child(1)").click();
-  await page.locator("button.MuiButton-text:nth-child(1)").click();
-  await page.locator("button.MuiTab-root:nth-child(1)").click();
-  const cell_name = await page.getByRole("cell", { name: "zmiana" });
-  const cell_surname = await page.getByRole("cell", { name: "nowe" });
-  await expect(cell_name).toBeVisible();
-  await expect(cell_surname).toBeVisible();
-
-  //kolejna zmiana
-  await page.locator("button.MuiTab-root:nth-child(1)").click();
-  await expect(page.getByRole("cell", { name: "Michal" })).toBeHidden();
-  await expect(page.getByRole("cell", { name: "Karbowanczyk" })).toBeHidden();
-  await page.locator("button.MuiTab-root:nth-child(2)").click();
-  await page.locator("#firstName").click();
-  await page.locator("#firstName").fill("Michal");
-  await page.locator("#lastName").click();
-  await page.locator("#lastName").fill("Karbowanczyk");
-  await page.locator("button.MuiButton-root:nth-child(1)").click();
-  await page.locator("button.MuiButton-text:nth-child(1)").click();
-  await page.locator("button.MuiTab-root:nth-child(1)").click();
-  const cell_name2 = await page.getByRole("cell", { name: "Michal" });
-  const cell_surname2 = await page.getByRole("cell", { name: "Karbowanczyk" });
-  await expect(cell_name2).toBeVisible();
-  await expect(cell_surname2).toBeVisible();
+test('editMyAccount', async ({ page }) => {
+  await page.goto('https://team-1.proj-sum.it.p.lodz.pl/');
+  await page.getByRole('menuitem', { name: 'Log in' }).getByRole('paragraph').click();
+  await page.getByLabel('Username*').click();
+  await page.getByLabel('Username*').fill('testParticipant');
+  await page.getByLabel('Password*').click();
+  await page.getByLabel('Password*').fill('P@ssw0rd');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('button').nth(1).click();
+  await page.getByText('Profil').click();
+  await page.getByRole('tab', { name: 'Change profile details' }).click();
+  await page.getByLabel('First Name*').click();
+  await page.getByLabel('First Name*').fill('Michał');
+  await page.getByRole('button', { name: 'save changes' }).click();
+  await page.getByRole('button', { name: 'Yes' }).click();
+  await page.getByRole('tab', { name: 'Profile details', exact: true }).click();
+  await expect(page.getByRole('cell', { name: 'Michał' })).toBeVisible();
+  await expect(page.getByRole('cell', { name: 'Maciej' })).toBeHidden();
+  await page.getByRole('tab', { name: 'Change profile details' }).click();
+  await page.getByLabel('First Name*').click();
+  await page.getByLabel('First Name*').fill('');
+  await page.getByLabel('First Name*').fill('Maciej');
+  await page.getByRole('button', { name: 'save changes' }).click();
+  await page.getByRole('button', { name: 'Yes' }).click();
+  await page.getByRole('tab', { name: 'Profile details', exact: true }).click();
+  await expect(page.getByRole('cell', { name: 'Maciej' })).toBeVisible();
 });
--- ssbd202401web/tests/test-MOK13editAccountByAdmin.spec.ts
+++ ssbd202401web/tests/test-MOK13editAccountByAdmin.spec.ts
@@ -1,46 +1,30 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("test", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
-  await page
-    .getByRole("menuitem", { name: "Log in" })
-    .getByRole("paragraph")
-    .click();
-  await page.getByLabel("Username*").click();
-  await page.getByLabel("Username*").fill("testAdmin");
-  await page.getByLabel("Password*").click();
-  await page.getByLabel("Password*").fill("P@ssw0rd");
-  await page.getByLabel("Password*").press("Enter");
-  await page.locator(".css-1l0r181 > li:nth-child(2) > p:nth-child(1)").click();
-  await page.locator("tr", { hasText: "halinaposwiatoswka12" }).click();
-  await page.locator("button.MuiTab-root:nth-child(2)").click();
-  await page.locator("#firstName").click();
-  await page.locator("#firstName").fill("zmiana");
-  await page.locator("#lastName").click();
-  await page.locator("#lastName").fill("nowe");
-  await page.locator("button.MuiButtonBase-root:nth-child(10)").click();
-  await page.locator(".MuiDialogActions-root > button:nth-child(1)").click();
-  await page.locator("button.MuiTab-root:nth-child(1)").click();
-  const cell_name = await page.getByRole("cell", { name: "zmiana" });
-  const cell_surname = await page.getByRole("cell", { name: "nowe" });
-  await expect(cell_name).toBeVisible();
-  await expect(cell_surname).toBeVisible();
-
-  //kolejna zmiana
-  await expect(page.getByRole("cell", { name: "Jacek" })).toBeHidden();
-  await expect(page.getByRole("cell", { name: "Bazant" })).toBeHidden();
-  await page.locator(".css-1l0r181 > li:nth-child(2) > p:nth-child(1)").click();
-  await page.locator("tr", { hasText: "halinaposwiatoswka12" }).click();
-  await page.locator("button.MuiTab-root:nth-child(2)").click();
-  await page.locator("#firstName").click();
-  await page.locator("#firstName").fill("Jacek");
-  await page.locator("#lastName").click();
-  await page.locator("#lastName").fill("Bazant");
-  await page.locator("button.MuiButtonBase-root:nth-child(10)").click();
-  await page.locator(".MuiDialogActions-root > button:nth-child(1)").click();
-  await page.locator("button.MuiTab-root:nth-child(1)").click();
-  const cell_name2 = await page.getByRole("cell", { name: "Jacek" });
-  const cell_surname2 = await page.getByRole("cell", { name: "Bazant" });
-  await expect(cell_name2).toBeVisible();
-  await expect(cell_surname2).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('https://team-1.proj-sum.it.p.lodz.pl/');
+  await page.getByRole('menuitem', { name: 'Log in' }).getByRole('paragraph').click();
+  await page.getByLabel('Username*').click();
+  await page.getByLabel('Username*').fill('testAdmin');
+  await page.getByLabel('Username*').press('Tab');
+  await page.getByLabel('Password*').fill('P@ssw0rd');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.getByRole('menuitem', { name: 'Accounts' }).getByRole('paragraph').click();
+  await page.getByRole('cell', { name: '0d3d8632-0ed6-4e43-8021-' }).click();
+  await page.getByRole('tab', { name: 'Change profile details' }).click();
+  await page.getByLabel('Name*', {exact: true}).click();
+  await page.getByLabel('Name*', {exact: true}).fill('');
+  await page.getByLabel('Name*', {exact: true}).fill('WandaPoZmianie');
+  await page.getByRole('button', { name: 'save changes' }).first().click();
+  await page.getByRole('button', { name: 'Yes' }).click();
+  await page.getByRole('tab', { name: 'Profile details', exact: true }).click();
+  await expect(page.getByRole('cell', { name: 'WandaPoZmianie' })).toBeVisible();
+  await expect(page.getByText('Wanda', { exact: true })).toBeHidden();
+  await page.getByRole('tab', { name: 'Change profile details' }).click();
+  await page.getByLabel('Name*', {exact: true}).click();
+  await page.getByLabel('Name*', {exact: true}).fill('');
+  await page.getByLabel('Name*', {exact: true}).fill('Wanda');
+  await page.getByRole('button', { name: 'save changes' }).first().click();
+  await page.getByRole('button', { name: 'Yes' }).click();
+  await page.getByRole('tab', { name: 'Profile details', exact: true }).click();
+  await page.getByRole('cell', { name: 'Wanda', exact: true }).click();
 });
